604da33d9df07cbcd1517fd4a02436216919e3cc
Krzysztof Potocki <kpotocki89@gmail.com>
2022-07-31 21:26:51 +0200

    Add bell signal at end of countdown

--- app/script.js
+++ app/script.js
@@ -7,7 +7,6 @@ const App = () => {
   const [time, setTime] = useState(0);
   const [timer, setTimer] = useState(null);
 
-  
   const formatTime = (time) => {
     let seconds = String(time % 60).padStart(2, '0');
     let minutes = String(Math.floor(time / 60)).padStart(2, '0');
@@ -32,9 +31,15 @@ const App = () => {
     window.close();
   }
 
+  const playBell = () => {
+    const bell = new Audio('./sounds/bell.wav');
+    bell.play();
+  };
+
   useEffect(() => {
     if(status !== 'off'){
       if (time === 0) {
+        playBell();
         if (status === 'work') {
           setStatus('rest');
           setTime(20);
@@ -48,24 +53,24 @@ const App = () => {
 
   return (
     <div>
-        <h1>Protect your eyes</h1>
-        { status === 'off' && (
-          <div>
-            <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
-            <p>This app will help you track your time and inform you when it's time to rest.</p>
-          </div>
-        )}
-        { status === 'work' && (<img src="./images/work.png" />)}
-        { status === 'rest' && (<img src="./images/rest.png" />)}
-        { status !== 'off' && (
-          <div className="timer">
-            { formatTime(time) }
-          </div>
-        )}
-        { status === 'off' && (<button className="btn" onClick={startTimer}>Start</button>)}
-        { status !== 'off' && (<button className="btn" onClick={stopTimer}>Stop</button>)}
-        <button className="btn btn-close" onClick={closeApp}>X</button>
-      </div>
+      <h1>Protect your eyes</h1>
+      { status === 'off' && (
+        <div>
+          <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
+          <p>This app will help you track your time and inform you when it's time to rest.</p>
+        </div>
+      )}
+      { status === 'work' && (<img src="./images/work.png" />)}
+      { status === 'rest' && (<img src="./images/rest.png" />)}
+      { status !== 'off' && (
+        <div className="timer">
+          { formatTime(time) }
+        </div>
+      )}
+      { status === 'off' && (<button className="btn" onClick={startTimer}>Start</button>)}
+      { status !== 'off' && (<button className="btn" onClick={stopTimer}>Stop</button>)}
+      <button className="btn btn-close" onClick={closeApp}>X</button>
+    </div>
   )
 }
 
